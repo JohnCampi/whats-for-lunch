c94a9524e99045ed569151c36c28250679f88ec3
JohnCampi <81334006+JohnCampi@users.noreply.github.com>
2023-01-04 18:50:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const select = document.querySelector("#priceSelect");
 const button = document.querySelector("button");
-const restList = document.querySelector("#restList");
-
-const mcdonalds = {
-  name: "McDonald's",
-  url: "https://www.mcdonalds.com/us/en-us.html",
-};
+const ul = document.querySelector("ul");
 
 const cheapTier = {
   mcdonalds: {
@@ -27,6 +22,14 @@ const cheapTier = {
   tacoBell: {
     name: "Taco Bell",
     url: "https://www.tacobell.com",
+  },
+  wiener: {
+    name: "Wienerschnitzel",
+    url: "https://www.wienerschnitzel.com/",
+  },
+  wendy: {
+    name: "Wendy's",
+    url: "https://www.wendys.com/",
   },
 };
 
@@ -55,6 +58,30 @@ const midTier = {
     name: "Panda Express",
     url: "https://www.pandaexpress.com",
   },
+  chipotle: {
+    name: "Chipotle",
+    url: "https://www.chipotle.com/",
+  },
+  bobsBurgers: {
+    name: "Bob's Burgers",
+    url: "https://www.bobsburgersnewmexico.com/",
+  },
+  goodFellas: {
+    name: "Goodfellas",
+    url: "https://www.goodfellaspizzaalbuquerque.com/",
+  },
+  laPlaya: {
+    name: "Mariscos La Playa",
+    url: "https://www.facebook.com/Mariscoslaplayaabq/",
+  },
+  Delicias: {
+    name: "Delicias Cafe",
+    url: "http://orderdeliciascafe.com/",
+  },
+  jerseyMike: {
+    name: "Jersey Mike's",
+    url: "https://www.jerseymikes.com/",
+  },
 };
 
 const highTier = {
@@ -64,10 +91,64 @@ const highTier = {
   },
   tds: {
     name: "Teds Diner",
+    url: "https://www.tdsnorth.com",
   },
 };
 
-const ul = document.querySelector("ul");
+const allTiers = {
+  mcdonalds: {
+    name: "McDonald's",
+    url: "https://www.mcdonalds.com/us/en-us.html",
+  },
+  burgerKing: {
+    name: "Burger King",
+    url: "https://www.bk.com/",
+  },
+  subway: {
+    name: "Subway",
+    url: "https://www.subway.com/en-US",
+  },
+  whataburg: {
+    name: "WHATABURGER",
+    url: "https://whataburger.com/home",
+  },
+  tacoBell: {
+    name: "Taco Bell",
+    url: "https://www.tacobell.com",
+  },
+  dions: {
+    name: "Dion's",
+    url: "https://www.dions.com/",
+  },
+  takoTen: {
+    name: "Tako Ten",
+    url: "https://www.selflane.com/bizs/tako-ten_albuquerque_nm",
+  },
+  chickFilA: {
+    name: `Chick-fil-A`,
+    url: "https://www.chick-fil-a.com/",
+  },
+  teriyaki: {
+    name: "Teriyaki Chicken Bowl",
+    url: "https://teriyakichickenbowl.com/",
+  },
+  tacoCabana: {
+    name: "Taco Cabana",
+    url: "https://www.tacocabana.com/",
+  },
+  pandaExpress: {
+    name: "Panda Express",
+    url: "https://www.pandaexpress.com",
+  },
+  sushi: {
+    name: "Sprouts Sushi",
+    url: "https://www.sprouts.com/",
+  },
+  tds: {
+    name: "Teds Diner",
+    url: "https://www.tdsnorth.com",
+  },
+};
 
 const urlLinks = (name, url) => {
   const link = document.createElement("a");
@@ -95,7 +176,6 @@ const mid = (value) => {
 };
 
 const high = (value) => {
-  ul.innerHTML = "";
   if (parseInt(select.value) === value) {
     for (let name in highTier) {
       urlLinks(highTier[name].name, highTier[name].url);
@@ -103,7 +183,23 @@ const high = (value) => {
   }
 };
 
+let entries;
+const array = [];
+let ranNum = Math.floor(Math.random() * 13) + 1;
+
+const rand = (value) => {
+  if (parseInt(select.value) === value) {
+    for (let all in allTiers) {
+      array.push(allTiers[all]);
+      ranNum = Math.floor(Math.random() * 13) + 1;
+    }
+    urlLinks(array[ranNum].name, array[ranNum].url);
+  }
+};
+
 button.addEventListener("click", () => {
+  ul.innerHTML = "";
+  rand(4);
   high(3);
   mid(2);
   cheap(1);
